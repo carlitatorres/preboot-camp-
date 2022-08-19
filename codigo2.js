f4e2874244a68863b258fc5dd19b0e83b832f10f
ignaciofa18 <ignaciofariasiccp@gmail.com>
2022-08-19 10:37:17 -0400

    subiendo preducciones

---
+++ codigo2.js
@@ -0,0 +1,17 @@
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+
+    var frozen = ["brócoli", "helado"];
+
+    frozen.push("croqueta de papa");
+
+    console.log(frozen);
+}
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+//creo un arreglo llamado movielibrary, ingreso dato "zoro" y despues reeplazo ET por Beetlejuice
